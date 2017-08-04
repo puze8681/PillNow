1a1dfcb25b961aaabb747c01a824275a80128765
puze8681 <rsef0601@naver.com>
2017-08-04 21:52:20 +0900

    setting server

---
+++ app/src/main/java/hoosasack/pillnow/Server/JSONService.kt
@@ -0,0 +1,10 @@
+package hoosasack.pillnow.Server
+
+import android.telecom.Call
+
+/**
+ * Created by parktaejun on 2017. 8. 4..
+ */
+public interface JSONService {
+
+}
---
+++ app/src/main/java/hoosasack/pillnow/Server/ServerLogin.kt
@@ -0,0 +1,14 @@
+package hoosasack.pillnow.Server
+
+/**
+ * Created by parktaejun on 2017. 8. 4..
+ */
+public class ServerLogin {
+    var id : String
+    var password : String
+
+    constructor(id: String, password: String) {
+        this.id = id
+        this.password = password
+    }
+}
