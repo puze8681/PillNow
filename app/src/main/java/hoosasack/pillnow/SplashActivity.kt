9365b12225a6e4469c35f038a44ab98b67f0e86b
puze8681 <rsef0601@naver.com>
2017-08-16 03:21:27 +0900

    permission

--- app/src/main/java/hoosasack/pillnow/SplashActivity.kt
+++ app/src/main/java/hoosasack/pillnow/SplashActivity.kt
@@ -12,6 +12,18 @@ import android.widget.LinearLayout
 import hoosasack.pillnow.Util.Font.FontActivity
 import kotlinx.android.synthetic.main.activity_splash.*
 import java.lang.Thread.sleep
+import com.google.android.gms.internal.d
+import android.Manifest.permission.MODIFY_PHONE_STATE
+import android.Manifest.permission.RECORD_AUDIO
+import android.Manifest.permission.INTERNET
+import android.Manifest.permission.READ_CONTACTS
+import android.Manifest.permission.READ_PHONE_STATE
+import android.os.Build
+import android.content.pm.PackageManager
+import android.support.v4.content.ContextCompat
+import android.Manifest.permission.CALL_PHONE
+import android.util.Log
+
 
 class SplashActivity : FontActivity() {
 
@@ -26,17 +38,35 @@ class SplashActivity : FontActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash)
 
+        //퍼미션
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED ||
+                    ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    Log.d("puze", "PERMISSION")
+                    requestPermissions(arrayOf<String>(android.Manifest.permission.INTERNET, android.Manifest.permission.ACCESS_FINE_LOCATION), 200)
+                    Log.d("puze", "initApp0")
+                    initApp()
+                }
+            } else {
+                Log.d("puze", "initApp1")
+                initApp()
+            }
+            initApp()
+        } else {
+            Log.d("puze", "initApp2")
+            initApp()
+        }
+    }
+
+    fun initApp() : Unit{
         startAnimations()
-
         intentRegister = Intent(this, RegisterActivity::class.java)
-
         btn_register.setOnClickListener{
             view->startActivity(intentRegister)
             finish()
         }
-
     }
-
     override fun onAttachedToWindow(): Unit{
         super.onAttachedToWindow()
         val window : Window = window
